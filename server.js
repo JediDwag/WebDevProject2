5096c60e955bdf31e7ea78815a91a19dcf11e14e
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-11-07 15:54:38 -0800

    Made pie chart for year template

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Built-in Node.js modules
 let fs = require('fs');
 let path = require('path');
+let Chart = require('chart.js');
 
 // NPM modules
 let express = require('express');
 let sqlite3 = require('sqlite3');
-
 
 let public_dir = path.join(__dirname, 'public');
 let template_dir = path.join(__dirname, 'templates');
@@ -40,8 +40,9 @@ app.get('/year/:selected_year', (req, res) => {
         // this will require a query to the SQL database
         
         //Put in selected year
-        template = template.replace("YEAR", req.params.selected_year);   
-        
+        template = template.replace("YEAR_HEADER", req.params.selected_year);   
+        template = template.replace("year", "year = " + req.params.selected_year);
+
         //Get data from database for the selected year
         let query = 'SELECT state_abbreviation, coal, natural_gas, nuclear, petroleum, renewable FROM Consumption WHERE year = ?';
         let params = [req.params.selected_year];
@@ -52,6 +53,11 @@ app.get('/year/:selected_year', (req, res) => {
 
             //Make table headers
             let table = "<table><tr><th>State</th><th>Coal Consumption</th><th>Gas Consumption</th><th>Nuclear Consumption</th><th>Petroleum Consumption</th><th>Renewable Consumption</th><th>Total Energy Consumption</th></tr>";
+            let coal_count = 0;
+            let natural_gas_count = 0;
+            let nuclear_count = 0;
+            let petroleum_count = 0;
+            let renewable_count = 0;
             var i = 0;
             for (i = 0; i < rows.length; i++) {
                 //Insert table rows
@@ -65,11 +71,25 @@ app.get('/year/:selected_year', (req, res) => {
                 let total = rows[i].coal + rows[i].natural_gas + rows[i].nuclear + rows[i].petroleum + rows[i].renewable;
                 table = table + "<td>" + total + "</td>";
                 table = table + "</tr>"
+
+                //Count up summary statistics
+                coal_count = coal_count + rows[i].coal;
+                natural_gas_count = natural_gas_count + rows[i].natural_gas;
+                nuclear_count = nuclear_count + rows[i].nuclear;
+                petroleum_count = petroleum_count + rows[i].petroleum;
+                renewable_count = renewable_count + rows[i].renewable;
             }
             table = table + "</table>";
             //Insert table into template
             template = template.replace("TABLE", table);
 
+            //Insert summary statistics into template
+            template = template.replace("coal_count", "coal_count = " + coal_count);
+            template = template.replace("natural_gas_count", "natural_gas_count = " + natural_gas_count);
+            template = template.replace("nuclear_count", "nuclear_count = " + nuclear_count);
+            template = template.replace("petroleum_count", "petroleum_count = " + petroleum_count);
+            template = template.replace("renewable_count", "renewable_count = " + renewable_count);
+            
             res.status(200).type('html').send(template); // <-- you may need to change this
         });
 
